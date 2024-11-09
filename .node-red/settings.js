9df913884e71c4dc5a37cb873332139248db1dfd
Jean-Baptiste MARCO <jean-baptiste.marco@ecole-air.fr>
2024-11-09 19:09:32 +0100

    Fonction de sauvegarde réalisée et améliration du Front-end

--- .node-red/settings.js
+++ .node-red/settings.js
@@ -418,6 +418,7 @@ module.exports = {
      */
     functionGlobalContext: {
         // os:require('os'),
+        fs: require('fs')
     },
 
     /** The maximum number of messages nodes will buffer internally as part of their
